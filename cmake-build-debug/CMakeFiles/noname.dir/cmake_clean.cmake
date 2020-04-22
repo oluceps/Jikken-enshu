b2b3bb25b109641d4f5b4f6d9a9a489ca788cb53
Brijiujiu <35628088+Brijiujiu@users.noreply.github.com>
2020-04-22 21:59:22 +0800

    Practice 20/4/22/b001

---
+++ cmake-build-debug/CMakeFiles/noname.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/noname.dir/main.cpp.obj"
+  "libnoname.dll.a"
+  "noname.exe"
+  "noname.exe.manifest"
+  "noname.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/noname.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
