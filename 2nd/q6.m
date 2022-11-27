7f371a2865881853586db9924b8b0cbd05b97ffa
oluceps <i@oluceps.uk>
2022-11-27 20:20:24 +0800

    fix: error on q6

--- 2nd/q6.m
+++ 2nd/q6.m
@@ -1,11 +1,15 @@
 n = 10;
 s = 0;
 
-for i = 1:n
-    s = s + 1 / i.^2;
+for i = 0:2:n
+    if i == 0
+        i = 1;
+    end
+    s = s + 1 / i;
 end
 s
 
 fprintf("<<<<<<<<<<<<<<<<< with vectorizasion >>>>>>>>>>>>>>>>>>>>>>\n")
-i = 1:n;
-sum(1./i.^2)
+i = 0:2:n;
+i(1) = 1;
+sum(1./i)
